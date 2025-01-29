daf1e69e2cc219d0d66e4002c24538880e273e5a
viola-1914 <inesmafo536@gmail.com>
2025-01-29 20:08:39 +0100

    Enregistrement de mes modifications avant de changer de branche

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # Generator_tree_ascII
-creation d'une classe Node, avec les attributs nom : qui represente le nom du noeud, vector<node*>children qui est un vecteur de pointeurs permettant stoker les pointeurs pointant vers les enfants.
-Fonction recusive printree()
-pour la creation le l'arbre avec idendation et lignes ascII pour montrer la relation entre les différents noeuds. Fonction principale main pour l'exécution.
-
-Ajout des fonctions pour ajouter, supprimer un noeud. Menu permettant l'ajout, la suppréssion d'un noeud.
-creation d'une chaine alimentaire .
- 
+Conception d'un programme en c/c++ pour construire une structure arborescente.
--- main.cpp
+++ main.cpp
@@ -16,36 +16,17 @@ public:
     void addChild(Node* child) {
         children.push_back(child);
     }
-
-    //supprimer un enfant par nom
-    bool supprimer_enfant(const string& nomEnfant){
-        for(size_t i=0; i<children.size(); i++){
-            if(children[i]->name ==nomEnfant) {
-            delete children[i]; //liberer la mémoire
-            children.erase(children.begin() + i); //supprimer de la liste
-            return true; // l'enfant est supprimer
-            }
-        }
-        return false; // l'enfant n'hesite pas 
-    }
 };
 
-// Fonction pour libérer la mémoire de l'arbre
-void deleteTree(Node* node) {
-    for (Node* child : node->children) {
-        deleteTree(child); // Appel récursif pour libérer les enfants
-    }
-    delete node; // Libérer le nœud actuel
-}
 // Fonction récursive pour afficher l'arbre avec indentation ASCII
 void printTree(Node* node, string prefix = "", bool isLast = true) {
     cout << prefix;
     if (isLast) {
-        cout << "\\-- ";
+        cout << "└── ";
         prefix += "    ";
     } else {
-        cout << "|-- ";
-        prefix += "|    ";
+        cout << "├── ";
+        prefix += "│   ";
     }
 
     cout << node->name << endl;
@@ -55,136 +36,29 @@ void printTree(Node* node, string prefix = "", bool isLast = true) {
     }
 }
 
-Node* findNode(Node* node, const string& name) {
-    if (node->name == name) {
-        return node;
-    }
-    for (Node* child : node->children) {
-        Node* result = findNode(child, name);
-        if (result != nullptr) {
-            return result;
-        }
-    }
-    return nullptr; // Nœud non trouvé
-}
-
-Node* addNode(Node* root) {
-    string parentName, childName;
-    cout << "Entrez le nom du parent sous lequel ajouter le nouveau nœud : ";
-    cin >> parentName;
-
-    Node* parentNode = findNode(root, parentName);
-    if (parentNode) {
-        cout << "Entrez le nom du nouveau nœud : ";
-        cin >> childName;
-        Node* newNode = new Node(childName);
-        parentNode->addChild(newNode);
-        return newNode;
-    } else {
-        cout << "Nœud parent '" << parentName << "' non trouvé." << endl;
-        return nullptr;
-    }
-}
-
-// Fonction pour ajouter un noeud
-//Node* addNode(Node* parent) {
-   // string name;
-   // cout << "Entrez le nom du nouveau nœud : ";
-   // cin >> name;
-   // Node* newNode = new Node(name);
-   // parent->addChild(newNode);
-   // return newNode;
-//}
-
-
-
-// supprimé un noeud
-void removeNode(Node* parent) {
-    string name;
-    cout << "Entrez le nom du nœud à supprimer : ";
-    cin >> name;
-    if (parent->supprimer_enfant(name)) {
-        cout << "Nœud '" << name << "' supprimé." << endl;
-    } else {
-        cout << "Nœud '" << name << "' non trouvé." << endl;
-    }
-}
-
 int main() {
-    // Création du noeud racine
-    Node* root = new Node("Chaine Alimentaire");
-
-
-    Node* Producteurs = new Node("Producteurs");
-    Node* Plantes = new Node("Plantes");
-    Node* Algues= new Node("Algues");
-    Node* Herbivores= new Node("Herbivores");
-    Node* Lapin= new Node("Lapin");
-    Node* Cerf= new Node("Cerf");
-    Node* Carnivores = new Node("Carnivores");
-    Node* Loup= new Node("Loup");
-    Node* Aigle= new Node("Aigle");
-
-    Node* Omnivores = new Node("Omnivores");
-    Node* Homme= new Node("Homme");
-    Node* Singe= new Node("Singe");
-
-
+    // Création des nœuds
+    Node* root = new Node("Racine");
+    Node* enfant1 = new Node("Enfant1");
+    Node* enfant2 = new Node("Enfant2");
+    Node* sousEnfant1 = new Node("SousEnfant1");
+    Node* sousEnfant2 = new Node("SousEnfant2");
 
     // Construction de l'arbre
-    Producteurs->addChild(Plantes);
-    Producteurs->addChild(Algues);
-    Herbivores->addChild(Lapin);
-    Herbivores->addChild(Cerf);
-    Omnivores->addChild(Homme);
-    Omnivores->addChild(Singe);
-    Carnivores->addChild(Loup);
-    Carnivores->addChild(Aigle);
-    root->addChild(Producteurs);
-    root->addChild(Herbivores);
-    root->addChild(Carnivores);
-    root->addChild(Omnivores);
+    enfant1->addChild(sousEnfant1);
+    enfant1->addChild(sousEnfant2);
+    root->addChild(enfant1);
+    root->addChild(enfant2);
 
+    // Affichage de l'arbre
+    printTree(root);
 
-
-
-      while (true)
-      {
-        /* code */
-     
-
-        cout << "\nMenu :\n";
-        cout << "1. Afficher l'arbre\n";
-        cout << "2. Ajouter un nœud\n";
-        cout << "3. Supprimer un nœud\n";
-        cout << "4. Quitter\n";
-        cout << "Choisissez une option : ";
-
-        int choice;
-        cin >> choice;
-
-        switch (choice) {
-            case 1:
-                printTree(root);
-                break;
-            case 2: {
-
-                
-               addNode(root);
-                break;
-                 }
-            case 3:
-                removeNode(root);
-                break;
-            case 4:
-                // Libération de la mémoire avant de quitter
-                 deleteTree(root); // Vous pouvez étendre cela pour libérer tous les nœuds
-                return 0;
-            default:
-                cout << "Choix invalide, veuillez réessayer." << endl;
-        }
-    }
-
+    // Libération de la mémoire (important pour éviter les fuites)
+    delete sousEnfant1;
+    delete sousEnfant2;
+    delete enfant1;
+    delete enfant2;
+    delete root;
 
     return 0;
 }
